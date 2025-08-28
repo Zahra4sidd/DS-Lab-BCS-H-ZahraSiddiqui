005748106218baf0961ed954f717544cb6040598
Zahra Siddiqui <siddiqui.zahra2006@gmail.com>
2025-08-28 14:34:24 +0500

    Create Q3.cpp

---
+++ Lab 01/Q3.cpp
@@ -0,0 +1,31 @@
+# include <iostream>
+using namespace std;
+
+class Box{
+    int *number;
+    public:
+    ~Box(){
+        delete number;
+    }
+    Box(int n){
+        number = new int(n);
+    }
+    Box(Box &other){    
+        number = new int(*other.number);   //deep copy
+    }
+    Box(Box &other, bool num){
+        number = other.number;  //shallow copy
+    }
+   Box& operator=(const Box& other){
+    if (this != &other) {             
+        delete number;                
+        number = new int(*other.number);  // copy assignment operator
+    return *this;
+}
+};
+
+int main(){
+    Box b1(10);
+    Box b2(20);
+    Box b3 = b1;
+}
